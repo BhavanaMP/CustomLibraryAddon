2f9723a5610fb97f0a4ab0f21b0bbe59fa0a3ee0
bhavana <malla.bhavana26@gmail.com>
2021-06-04 10:26:16 +0200

    WEEK1: Initial commit of Custom Lib addon internal folders skeleton

---
+++ custom/lib_portal/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name' : 'Library Portal',
+    'version' : '1.1',
+    'author' : 'bloopark', 
+    'summary': 'Library Management System',
+    'description': """
+    Module to digitalise and manage the library operations
+    """,
+    'depends' : ['base'],
+    'sequence' : 1,
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'category' : 'Library',
+    'data' : [],
+
+}
---
+++ custom/lib_portal/models/__init__.py
@@ -0,0 +1,3 @@
+# from . import res_partner
+from . import lib_book_lending
+
